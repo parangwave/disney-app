43a5b7d8b748ba306befae40f1a0296c13d5c7de
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-07 01:43:46 +0900

    feat: Add router setting
    router 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyle from "./utils/GlobalStyle";
+import router from "./router";
 
 // theme
 import { ThemeProvider } from "styled-components";
@@ -19,6 +21,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={true} />
       </ThemeProvider>
     </QueryClientProvider>
---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Character from "./pages/Character";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: ":id",
+        element: <Character />,
+      },
+    ],
+  },
+]);
+export default router;
