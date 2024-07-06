ebab7d899b04f4a863a76846f5f55df04e9bafe0
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-06 18:49:19 +0900

    init: Init project
    프로젝트 초기화

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// theme
+import { ThemeProvider } from "styled-components";
+import { theme } from "./utils/theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const queryClient = new QueryClient();
+
+root.render(
+  <>
+    <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </>
+);
