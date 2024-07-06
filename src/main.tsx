909058f04614f0623ee274743736a80e38c490ef
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-07 01:36:18 +0900

    feat: Add GlobalStyle & theme
    GlobalStyle과 theme 생성

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyle from "./utils/GlobalStyle";
 
 // theme
 import { ThemeProvider } from "styled-components";
@@ -16,7 +17,10 @@ const queryClient = new QueryClient();
 root.render(
   <>
     <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
     </QueryClientProvider>
   </>
 );
