a9bad03dce982eed63eb8078431544a780cb55e5
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-07 18:14:16 +0900

    design: Redesign NotFound
    NotFound의 css 수정함; Home으로 돌아가는 링크 hover시 효과 추가

--- src/component/NotFound.tsx
+++ src/component/NotFound.tsx
@@ -2,20 +2,34 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NotFoundCotainer = styled.div`
-  height: 90vh;
+  height: 70vh;
   display: flex;
   flex-direction: column;
   text-align: center;
   justify-content: center;
   align-items: center;
   font-size: 5rem;
+
+  a {
+    margin: 3rem 0;
+    transition: all 0.3s ease-in-out;
+    font-size: 3rem;
+
+    span {
+      display: inline-block;
+    }
+
+    &:hover {
+      font-size: 3.5rem;
+    }
+  }
 `;
 export default function NotFound() {
   return (
     <NotFoundCotainer>
       <Link to="/">
-        <span>←</span>
-        Back to Home
+        <span>←&nbsp;</span>
+        <span>Back to Home</span>
       </Link>
       <h1>Not Found 404</h1>
     </NotFoundCotainer>
