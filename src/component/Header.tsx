eb879e2e03fbe16de4dd649e23e93fc579e03a59
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-07 18:02:35 +0900

    feat: Add Header
    Header 컴포넌트 추가

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,3 +1,29 @@
+import styled from "styled-components";
+import logoImg from "../assets/disney-logo.svg";
+
+const HeaderContainer = styled.h1`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem 0;
+
+  img {
+    width: 15rem;
+    filter: invert(100%);
+  }
+
+  h3 {
+    padding: 1.5rem 0;
+    font-size: 2.5rem;
+  }
+`;
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <HeaderContainer>
+      <img src={logoImg} alt="header" />
+      <h3>Meet ALL Disney Characters</h3>
+    </HeaderContainer>
+  );
 }
