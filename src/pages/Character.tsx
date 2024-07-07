35dea948bf0b6c14efe8bc226fa2b28b7abea4b3
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-08 04:03:54 +0900

    design: Deisgn Character
    Character 컴포넌트 css 작업

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -4,7 +4,51 @@ import { useParams } from "react-router-dom";
 import { ICharacterInfo } from "./../utils/characterInfo";
 import { Link } from "react-router-dom";
 import Loader from "../component/Loader";
+import styled from "styled-components";
 import { ProfileImg } from "../component/ProfileImg";
+
+const CharacterProfile = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 3rem;
+    margin: 2rem;
+  }
+
+  a {
+    margin-top: 2rem;
+    font-size: 2rem;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
+
+const Movies = styled.ul`
+  width: 30vw;
+
+  li {
+    display: inline-block;
+    background-color: #1e272e;
+    padding: 1rem;
+    font-size: 1.2rem;
+    align-text: center;
+    border-radius: 20px;
+    transition: all 0.2s ease-in-out;
+
+    &:not(last-child) {
+      margin: 0.7rem;
+    }
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
 
 export default function Character() {
   const { id = " " } = useParams<{ id: string }>();
@@ -16,24 +60,23 @@ export default function Character() {
   });
 
   return (
-    <>
+    <main>
       {isLoading ? (
         <Loader />
       ) : (
-        <>
-          <img src={`${data?.imageUrl}`} alt="character profile img" />
+        <CharacterProfile>
           <ProfileImg>
             <img src={`${data?.imageUrl}`} alt="character profile img" />
           </ProfileImg>
           <h2>{data?.name}</h2>
-          <ul>
+          <Movies>
             {data?.films.map((film, idx) => (
               <li key={idx}>{film}</li>
             ))}
-          </ul>
+          </Movies>
           <Link to={data?.sourceUrl ?? ""}>More Info in Fandom Wiki</Link>
-        </>
+        </CharacterProfile>
       )}
-    </>
+    </main>
   );
 }
