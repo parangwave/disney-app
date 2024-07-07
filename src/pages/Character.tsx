6d3f8226fc454d85b0b1582e6873263f2ac8f65b
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-07 17:51:05 +0900

    feat: Add queries in Home & Chracter
    Home, Chracter 컴포넌트에 api에서 정의한 함수를 통해 data fetch하여 display하도록 함

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,3 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCharacterInfo } from "../utils/api";
+import { useParams } from "react-router-dom";
+import { ICharacterInfo } from "./../utils/characterInfo";
+import { Link } from "react-router-dom";
+
 export default function Character() {
-  return <h1>Chracter</h1>;
+  const { id = " " } = useParams<{ id: string }>();
+
+  const { isLoading, data } = useQuery<ICharacterInfo>({
+    queryKey: ["characterInfo", id],
+    queryFn: () => fetchCharacterInfo(id),
+    enabled: id !== "", // id가 빈 문자열이 아닐 때만 쿼리 실행
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <h3>Loading</h3>
+      ) : (
+        <>
+          <img src={`${data?.imageUrl}`} alt="character profile img" />
+          <h2>{data?.name}</h2>
+          <ul>
+            {data?.films.map((film, idx) => (
+              <li key={idx}>{film}</li>
+            ))}
+          </ul>
+          <Link to={data?.sourceUrl ?? ""}>More Info in Fandom Wiki</Link>
+        </>
+      )}
+    </>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCharacterList } from "../utils/api";
+import { ICharacterList } from "../utils/chatacterList";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const { isLoading, data } = useQuery<ICharacterList>({
+    queryKey: ["chracterList"],
+    queryFn: () => fetchCharacterList(),
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <h2>Loading</h2>
+      ) : (
+        <ul>
+          {data?.map((charater) => (
+            <li key={charater.id}>
+              <img src={charater.imageUrl} alt="character profile img" />
+              <Link to={`character/${charater.id}`}>{charater.name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
